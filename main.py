068df452eda8c2080c1007bee265f122f239f7e9
DayWD <DayWeed@github.com>
2021-06-12 17:55:14 +0200

    First 4 generators done.

--- main.py
+++ main.py
@@ -6,52 +6,65 @@ from sympy.ntheory.factor_ import antidivisors
 # from scipy.stats import chisquare
 # from statsmodels.sandbox.stats.runs import runstest_1samp
 
-
+# DON'T TOUCH~~~~~~~~~
 multiplier = 1597
 c = 51749
 m = 244944
+# ~~~~~~~~~~~~~~~~~~~~
 
-# counter = 20
 seed = 1
-
-
-def G1(seed):
-    return (multiplier * seed + c) % m
 
 
 def G(counter):
     tab = [seed]
     for i in range(0, counter):
         tab.append((multiplier * tab[i] + c) % m)
+    seed = tab[0]
     tab.pop(0)
     return tab
 
 
-def J(a, b, tab):
-    return (seed / m) * (b - a) + a
+def J(a, b, amount):
+    tab = G(amount)
+    for x in range(len(tab)):
+        tab[x] = (tab[x] / m) * (b - a) + a
+    return tab
 
 
-def B(x):
-    x = J(0, 1)
-    if x <= 0.5:
-        return 1
-    else:
-        return 0
+def B(p, amount):
+    tab = J(0, 1, amount)
+    for x in range(len(tab)):
+        if tab[x] <= p:
+            tab[x] = 1
+        else:
+            tab[x] = 0
+    return tab
 
 
-def D(counter, seed):
-    score = 0
-    for i in range(counter):
-        seed = G(seed)
-        print(B(seed))
-        if B(seed) == 1:
-            print(i)
-            score += 1
-    return score
+def D(p, amount_of_tests):
+    tab = B(p, amount_of_tests)
+    success = 0
+    for x in range(len(tab)):
+        if tab[x] == 1:
+            success += 1
+    return success
 
-asd = G(20)
-for i in asd:
-    print(i)
+def P(p,amount_of_tests):
+    L = p*amount_of_tests
+    L = math.exp(-L)
+    k = 0
+    while p>L:
+        k += 1
+        p *= 1
+
+print(G(1))
+print(G(1))
+print(G(1))
+
+# asd = B(0.5,30)
+# for i in asd:
+#     print(i)
+# P(0.5,200)
 
 #   Linear congruential generator
 # for i in range(counter):
